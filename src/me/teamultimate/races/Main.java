1fa9832330c9fd0d2f04fce4134c071b2f69fdbd
Xype <Paister@Paister-PC.fritz.box>
2012-09-16 12:15:18 +0200

    Fetched Dateien;

--- src/me/teamultimate/races/Main.java
+++ src/me/teamultimate/races/Main.java
@@ -1,10 +1,27 @@
 package me.teamultimate.races;
 
-public class Main {
+import java.util.HashMap;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
   
+public class Main extends JavaPlugin implements Listener{
+	
+  //Variablen
+	HashMap<String,rspieler> rplayers = new HashMap<String, rspieler>();
+  //ende
+	
  public void onEnable()
   {
-   System.out.println("Races 0.0.3 Enabled");
-   
+    String[] spieler = this.getConfig().getString("variables.players").split(",");
+    for(int i = 0 ; i < spieler.length ; i++)
+     {
+       String rasse = this.getConfig().getString("players." + spieler[i] + ".race");
+       int level = this.getConfig().getInt("players." + spieler[i] + ".level");
+       int exp = this.getConfig().getInt("players." + spieler[i] + ".exp");
+       
+       rplayers.put(spieler[i], new rspieler(rasse,exp,level));
+     }
+    System.out.println("Races 0.0.1 Enabled");
   }
+ 
 }
