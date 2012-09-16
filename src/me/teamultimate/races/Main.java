0a8130edc0b6907a3d6b58e7d9788afac2a63beb
Ch4t4r <flyffassistman@gmx.de>
2012-09-16 10:25:17 +0200

    config und plugin.yml hinzugefügt

--- src/me/teamultimate/races/Main.java
+++ src/me/teamultimate/races/Main.java
@@ -1,22 +1,26 @@
 package me.teamultimate.races;
 
 import java.util.HashMap;
-
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin implements Listener{
 	
   //Variablen
-	HashMap<String,rspieler> spieler = new HashMap<String, rspieler>();
+	HashMap<String,rspieler> rplayers = new HashMap<String, rspieler>();
   //ende
 	
  public void onEnable()
   {
     String[] spieler = this.getConfig().getString("variables.players").split(",");
+    
     for(int i = 0 ; i < spieler.length ; i++)
      {
-          	
+       String rasse = this.getConfig().getString("players." + spieler[i] + ".race");
+       int level = this.getConfig().getInt("players." + spieler[i] + ".level");
+       int exp = this.getConfig().getInt("players." + spieler[i] + ".exp");
+       
+       rplayers.put(spieler[i], new rspieler(rasse,exp,level));
      }
     
     System.out.println("Races 0.0.1 Enabled");
