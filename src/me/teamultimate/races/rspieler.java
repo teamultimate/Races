a43c48d667de632cbc3a4e83635568e7b42990ce
Ch4t4r <flyffassistman@gmx.de>
2012-09-16 14:37:24 +0200

    testing

--- src/me/teamultimate/races/rspieler.java
+++ src/me/teamultimate/races/rspieler.java
@@ -4,6 +4,7 @@ public class rspieler{
 	private int _level;
 	private int _experience;
 	private String _race;
+	private boolean initilzed;
 	public rspieler(String race,int exp,int lvl){
 		_level = lvl;
 		_experience = exp;
