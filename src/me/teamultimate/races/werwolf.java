188805ef6c0ee626022ec7119eab91132e71196a
Ch4t4r <flyffassistman@gmx.de>
2012-09-20 13:47:26 +0200

    Test wegen .class nicht hochgeladen! Minimale änderung

--- src/me/teamultimate/races/werwolf.java
+++ src/me/teamultimate/races/werwolf.java
@@ -1,11 +1,19 @@
 package me.teamultimate.races;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class werwolf implements Listener {
 private Main plugin;
 	public werwolf(Main instance){
 		this.plugin = instance;
 	    this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
+	}
+	
+	@EventHandler(priority = EventPriority.LOW)
+	public void onPlayerDamage(EntityDamageByEntityEvent e){
+		
 	}
 }
