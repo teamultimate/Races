13194d622f14226dd4d009cf4f641fc72d5cd893
Ch4t4r <flyffassistman@gmx.de>
2012-09-20 13:42:27 +0200

    rspieler Klasse hat nun get/set für rasse,level,exp

--- src/me/teamultimate/races/Main.java
+++ src/me/teamultimate/races/Main.java
@@ -25,7 +25,7 @@ public class Main extends JavaPlugin implements Listener{
        int level = this.getConfig().getInt("players." + spieler[i] + ".level");
        int exp = this.getConfig().getInt("players." + spieler[i] + ".exp");
        this.getServer().getPluginManager().registerEvents(this, this);
-       rplayers.put(spieler[i], new rspieler(rasse,exp,level));
+       rplayers.put(spieler[i], new rspieler(spieler[i],rasse,exp,level,this));
      }
     System.out.println("Races 0.0.1 Enabled");
   }
--- src/me/teamultimate/races/rspieler.java
+++ src/me/teamultimate/races/rspieler.java
@@ -4,9 +4,47 @@ public class rspieler{
 	private int _level;
 	private int _experience;
 	private String _race;
-	public rspieler(String race,int exp,int lvl){
+	private Main plugin;
+	private String _name;
+	
+	public rspieler(String name,String race,int exp,int lvl,Main instance){
 		_level = lvl;
 		_experience = exp;
 		_race = race;
-	}
+		this.plugin = instance;
+		_name = name;
+	}//#########
+	
+	public String race()
+	{
+	return _race;
+	}//#########
+	
+	public int level()
+	{
+	return _level;
+	}//#########
+	
+	public int exp()
+	{
+	return _experience;
+	}//#########
+	
+	public void setrace(String race)
+	{
+	_race = race;
+	this.plugin.getConfig().set("spieler." + _name + ".rasse", race);
+	}//#########
+
+	public void setlevel(int level)
+	{
+	_level = level;
+	this.plugin.getConfig().set("spieler." + _name + ".level", level);
+	}//#########
+	
+	public void setexp(int exp)
+	{
+	this.plugin.getConfig().set("spieler." + _name + ".exp", exp);
+	_experience = exp;
+	}//#########
 }
