a9473811f94380b3ee6178138fa12405f3c11359
Ch4t4r <flyffassistman@gmx.de>
2012-09-21 17:57:34 +0200

    Code für 4 % Critical fertig; Kompltee kommentiert todo: bei nacht 7 %;

--- src/me/teamultimate/races/Werwolf.java
+++ src/me/teamultimate/races/Werwolf.java
@@ -16,12 +16,21 @@ private Main plugin;
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerDamage(EntityDamageByEntityEvent e){
-		if(e.getDamager() instanceof Player){
-			Player p = (Player) e.getDamager();
-			int random = (int) (Math.random()*100+1);
-			if(random < 5){
-				e.setCancelled(true);
-			}
+		if(e.getDamager() instanceof Player){ //Ist der Angreifer ein Spieler?
+			Player p = (Player) e.getDamager(); //Spieler p = Angreifer
+			int random = (int) (Math.random()*100+1); //Zufallszahl zwischen 1 und 100
+			if(random < 5)
+			 { // wenn zufallszahl unter 5 : ca 4 %
+				if(e.getEntity() instanceof Player) //Wenn der geschädigte Mensch ist...
+				 {
+					Player s = (Player) e.getEntity();//Spieler s = Geschädigter
+					
+					if(s.getHealth() > 2) //Wenn spieler mehr als 2 (damit 3 oder höher!) Leben hat.. wir wollen ihm ja 2 abziehn ^^
+					{
+						p.setHealth(p.getHealth() - 2); // 2 Leben abziehen
+					}
+				 }
+			 }
 		}
 	}
 }
