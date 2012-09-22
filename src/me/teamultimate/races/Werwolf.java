073213668adfa3aaf53d1e6360c8726baa2bb956
Ch4t4r <flyffassistman@gmx.de>
2012-09-22 21:00:46 +0200

    Bei mir neu aufgesetzt

--- src/me/teamultimate/races/Werwolf.java
+++ src/me/teamultimate/races/Werwolf.java
@@ -9,13 +9,15 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class Werwolf implements Listener {
 private Main plugin;
-	public Werwolf(Main instance){
+	public Werwolf(Main instance)
+	{
 		this.plugin = instance;
 	    this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
-	public void onPlayerDamage(EntityDamageByEntityEvent e){
+	public void onPlayerDamage(EntityDamageByEntityEvent e)
+	{
 		if(e.getDamager() instanceof Player){ //Ist der Angreifer ein Spieler?
 			Player p = (Player) e.getDamager(); //Spieler p = Angreifer
 			int random = (int) (Math.random()*100+1); //Zufallszahl zwischen 1 und 100
@@ -25,7 +27,7 @@ private Main plugin;
 				 {
 					Player s = (Player) e.getEntity();//Spieler s = Geschädigter
 					
-					if(s.getHealth() > 2) //Wenn spieler mehr als 2 (damit 3 oder höher!) Leben hat.. wir wollen ihm ja 2 abziehn ^^
+					if(s.getHealth() > 2   &&  p.getWorld().getTime() > 14000 ) //Wenn spieler mehr als 2 (damit 3 oder höher!) Leben hat.. wir wollen ihm ja 2 abziehn ^^
 					{
 						p.setHealth(p.getHealth() - 2); // 2 Leben abziehen
 					}
